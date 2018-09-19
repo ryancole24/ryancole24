af6039baf1e345c5ddc1cad34830b7e457735abe
Ryan Cole <coler@appstate.edu>
2018-09-19 14:40:36 -0400

    SFNO test

--- Rscripts/junk.R
+++ Rscripts/junk.R
@@ -2,4 +2,4 @@
 
 hist(rnorm(10000), col - "purple")
 
-parks <- c("Dollywood", "Six Flags Over Georgia", "Carowinds")
+parks <- c("Dollywood", "Six Flags Over Georgia", "Carowinds", "Six Flags New Orleans")
